b195bd77ce8050228417b4bdc4be69151d4df1da
ShubhamEng99 <shubham.eng1999@gmail.com>
2025-08-16 17:22:07 +0530

    setup express server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('test')
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    return res.end('res')
+})
+
+app.listen(3000,(err)=>{
+    console.log('server is running on 3000')
+})
